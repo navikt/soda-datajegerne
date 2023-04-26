55371149e3070ebb585d572369291fdb8b88723c
Erik Vattekar <erik.vattekar@nav.no>
2023-04-26 11:00:37 +0200

    format slack message

--- run.py
+++ run.py
@@ -10,11 +10,20 @@ def run_scan(s: Scan, f: str, dir: str) -> list:
     s.execute()
     return [e.get_dict() for e in s.get_checks_warn_or_fail()]
 
+def create_slack_block(e: dict) -> dict:
+    return {
+        "type": "section",
+        "text": {
+            "type": "plain_text",
+            "text": f"Test '{e['name']}' fails for table {e['table']} (datasource {e['dataSource']})"
+        }
+    }
+
 def post_slack_message(errors: list) -> None:
     slack_token = os.environ["SLACK_TOKEN"]
     slack_channel = os.environ["SLACK_CHANNEL"]
     client = WebClient(token=slack_token)
-    client.chat_postMessage(channel=slack_channel if slack_channel.startswith("#") else "#"+slack_channel, text=json.dumps(errors))
+    client.chat_postMessage(channel=slack_channel if slack_channel.startswith("#") else "#"+slack_channel, blocks=[create_slack_block(e) for e in errors])
 
 if __name__ == "__main__":
     s = Scan()
